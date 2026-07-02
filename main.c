1025feda794ff94e64d856f44984ab9b49be53e9
Lucas Ruan <lucasdainn@gmail.com>
2026-07-02 11:17:37 -0300

    cria funcao lerOpcaoMenu

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int lerInteiroNoIntervalo (int minimo, int maximo);
+int lerOpcaoMenu(void);
 
 int main(){
 
@@ -30,4 +31,26 @@ int lerInteiroNoIntervalo (int minimo, int maximo){
     } while(entradaInvalida);
 
     return valor;
+}
+int lerOpcaoMenu(void){
+    int opcao;
+
+    printf(
+        "\n"
+        "FUNCIONARIOS\n"
+        "----------------------------------------\n"
+        "\n"
+        "  [1] Cadastrar funcionario\n"
+        "  [2] Listar funcionarios\n"
+        "  [3] Buscar por nome\n"
+        "  [4] Mostrar maior salario\n"
+        "  [5] Calcular media salarial\n"
+        "  [6] Sair\n"
+        "\n"
+        "Escolha uma opcao > "
+);
+
+    opcao = lerInteiroNoIntervalo(1, 6);
+    
+    return opcao;
 }
