47682a4791d31667cfe3a47ceb0efd0c098ab0cb
Lucas Ruan <lucasdainn@gmail.com>
2026-07-02 10:59:27 -0300

    cria função lerInteiroNoIntervalo

--- main.c
+++ main.c
@@ -1,6 +1,33 @@
 #include <stdio.h>
 
+int lerInteiroNoIntervalo (int minimo, int maximo);
+
 int main(){
 
     return 0;
+}
+
+int lerInteiroNoIntervalo (int minimo, int maximo){
+    int resultadoLeitura, valor, entradaInvalida;
+
+    do
+    {   
+        resultadoLeitura = scanf("%d", &valor);
+
+        if (resultadoLeitura != 1){
+            entradaInvalida = 1;
+            printf("Entrada invalida\n");
+            while(getchar() != '\n');
+        }
+
+        else {
+            entradaInvalida = (valor > maximo || valor < minimo);
+            if (entradaInvalida) {
+                printf("Valor invalido\n");
+            }
+        }
+
+    } while(entradaInvalida);
+
+    return valor;
 }
