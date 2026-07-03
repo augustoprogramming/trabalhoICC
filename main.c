162f6683461c6e0b1996e5b40fbcc3f6714ffbea
Lucas Emanuel <lukas12carneiro@gmail.com>
2026-07-03 13:16:58 -0300

    Criar a função mostrar funcionários, add case 2

--- main.c
+++ main.c
@@ -2,9 +2,19 @@
 
 int lerInteiroNoIntervalo (int minimo, int maximo);
 int lerOpcaoMenu(void);
+int cadastrarNome(char nome[][50], int indice);
+int cadastrarEmail(char email[][30], int indice);
+int cadastrarSalario(float salario[], int indice);
+float SalarioValido(void);
+void mostrarFuncionarios(char nome[][50], char email[][30], float salario[], int funcionarios);
+
 
 int main(){
     int opcao;
+    char nome[10][50];
+    char email[10][30];
+    float salario[10];
+    int funcionarios = 0;
 
     do
     {
@@ -12,11 +22,26 @@ int main(){
     
         switch (opcao){
             case 1:
-                // funcao cadastrar funcionario
+                if(funcionarios == 10){
+                    printf("AGENDA CHEIA! Por favor escolha outra opcao!!!\n");
+                    break;
+                }
+
+                int verificar_nome = cadastrarNome(nome, funcionarios);
+                int verificar_email = cadastrarEmail(email, funcionarios);
+                int verificar_salario = cadastrarSalario(salario, funcionarios);
+
+                if (verificar_nome && verificar_email && verificar_salario) {
+                    funcionarios++;
+                    printf("Funcionario cadastrado com sucesso!\n");
+                } else {
+                    printf("Falha no cadastro. Tente novamente.\n");
+                }
                 break;
 
             case 2:
                 // funcao mostrar funcionários
+                mostrarFuncionarios(nome, email, salario, funcionarios);
                 break;
 
             case 3:
@@ -39,7 +64,7 @@ int main(){
                 printf("Erro inesperado\n");
                 break;
         }
-            
+
     } while (opcao != 6);
 
     return 0;
@@ -57,7 +82,6 @@ int lerInteiroNoIntervalo (int minimo, int maximo){
             printf("Entrada invalida\n");
             while(getchar() != '\n');
         }
-
         else {
             entradaInvalida = (valor > maximo || valor < minimo);
             if (entradaInvalida) {
@@ -69,6 +93,7 @@ int lerInteiroNoIntervalo (int minimo, int maximo){
 
     return valor;
 }
+
 int lerOpcaoMenu(void) {
     int opcao;
 
@@ -90,4 +115,72 @@ int lerOpcaoMenu(void) {
     opcao = lerInteiroNoIntervalo(1, 6);
     
     return opcao;
+}
+
+int cadastrarNome(char nome[][50], int indice){
+    printf("Nome do funcionario: ");
+    int resultado = scanf(" %49[^\n]", nome[indice]);
+
+    if (resultado != 1) {
+        printf("Nome invalido.\n");
+        while (getchar() != '\n');
+        return 0;
+    }
+
+    return 1;
+}
+
+int cadastrarEmail(char email[][30], int indice){
+    printf("E-mail do funcionario: ");
+    int resultado = scanf(" %29[^\n]", email[indice]);
+
+    if (resultado != 1) {
+        printf("E-mail invalido.\n");
+        while (getchar() != '\n');
+        return 0;
+    }
+
+    return 1;
+}
+
+int cadastrarSalario(float salario[], int indice){
+    salario[indice] = SalarioValido();
+    return 1;
+}
+
+float SalarioValido(void){
+    float valor;
+    int resultadoLeitura, entradaInvalida;
+
+    do
+    {
+        printf("Salario do funcionario: ");
+        resultadoLeitura = scanf("%f", &valor);
+
+        if (resultadoLeitura != 1) {
+            entradaInvalida = 1;
+            printf("Entrada invalida (deve ser um numero)\n");
+            while (getchar() != '\n');
+        }
+        else {
+            entradaInvalida = (valor < 0);
+            if (entradaInvalida) {
+                printf("Salario nao pode ser negativo\n");
+            }
+        }
+
+    } while (entradaInvalida);
+
+    return valor;
+}
+void mostrarFuncionarios(char nome[][50], char email[][30], float salario[], int funcionarios) {
+    if (funcionarios == 0){
+        printf("\nNenhum funcionario cadastrado ainda.\n");
+        return;
+    }
+    printf("\n %-4s %-30s %-25s %-s\n", "Ind", "Nome", "E-mail", "Salario"); //imprime um aalgo, ocupando um espaço de N caracteres, alinhada à esquerda (o '-' faz isso, sem o '-', alinharia à direita)
+                                                                           // esses números garantem que as colunas fiquem alinhadas 
+    for(int i = 0; i < funcionarios; i++){
+         printf("%-4d %-30s %-25s %.2f\n", i + 1, nome[i], email[i], salario[i]);
+    }
 }
